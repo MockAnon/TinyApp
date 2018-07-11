b28ea2a687fd41336a1d8c1f291dc2b081eafc4f
Jaron D Evans <jarondevans@gmail.com>
2018-07-11 23:50:03 +0000

    I added a login/logout function

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
 
 app.set("view engine", "ejs");
 
@@ -17,6 +19,19 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username); //task 5 add cookie username [, domain]
+
+
+  res.redirect("/urls");
+
+
+
+});
+
+
 
 
 app.post("/urls/:id/change", (req, res) => {  //part 3 change url
@@ -39,31 +54,48 @@ app.post("/urls/:id/delete", (req, res) => {  //part 3 delete
 });
 
 
+
+app.get("/logout", (req, res) => {
+  // res.clearCookie(username);
+
+  console.log(req.cookies["username"]);
+  res.clearCookie("username");
+  res.redirect("/");
+
+});
+
 app.get("/", (req, res) => {
-  res.render("urls_new");
+  console.log(req.cookies)
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL:urlDatabase[req.params.id], urls: urlDatabase};
+  let templateVars = { shortURL: req.params.id, longURL:urlDatabase[req.params.id], urls: urlDatabase,
+  username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
+});
+
+
 
 
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL); //part 2 ---this is what redirects people to submitted webpage.
 });
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-
 // app.get("/urls/new", (req, res) => {
 //   res.render("urls_new");
 // });
